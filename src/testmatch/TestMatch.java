3e13bdae53d3b3139c9ad2943d2befab9f6dde91
boltoth <boltoth@Toms-Mac-mini.local>
2016-02-06 18:58:06 -0800

    new files

---
+++ src/testmatch/TestMatch.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testmatch;
+
+/**
+ *
+ * @author boltoth
+ */
+public class TestMatch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String string = "AMAZON MKTPLACE PMTS 440 Terry Ave N AMZN.";
+        //String exp = "^AMAZON.*";
+        String exp = ".*amazon.*";
+        //String exp = ".*amazon.*|.*AMAZON.*|.*440.*";
+        System.out.println("Value=" + string.toLowerCase().matches(exp));
+    }
+    
+}
